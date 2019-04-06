9cf31ab6066c647d722b218845fc3bb8711b5752
Christopher Gwizdala <chrgwizd@publicisgroupe.net>
2019-04-05 17:26:54 -0700

    Ignore .git folder after clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" -av --no-perms . ~
+		--exclude "README.md" --exclude ".git" -av --no-perms . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
