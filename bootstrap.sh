4793aa466515e1606d334aab00166a9467804d45
Christopher Gwizdala <chris.gwizdala@pluto.tv>
2019-04-16 19:37:17 -0700

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-	rsync --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude ".git" -av --no-perms . ~
+	rsync --exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude ".git" \
+		-av --no-perms . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
@@ -18,7 +21,7 @@ unset doIt
 
 if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
     # assume Zsh
-    source ~/.zshrc
+    source ~/.zprofile
 elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     # assume Bash
     source ~/.bash_profile
