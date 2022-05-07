4053d2a3122ce59279f705cd2a13b4ed515eada7
Christopher Gwizdala <597282+christowiz@users.noreply.github.com>
2022-05-06 17:26:57 -0700

    permissions fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
+TMP_DIR=tmp
+
 git pull origin master
 function doIt() {
 	rsync --exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude ".git" \
-		-av --no-perms --omit-dir-times . ~
+		-av --no-perms --omit-dir-times . tmp
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
@@ -15,14 +16,17 @@ else
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
+
 	fi
 fi
 unset doIt
 
-if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-    # assume Zsh
-    source ~/.zprofile
-elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-    # assume Bash
-    source ~/.bash_profile
+cp -R tmp/. ~/
+
+if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
+	# assume Zsh
+	source ~/.zprofile
+elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
+	# assume Bash
+	source ~/.bash_profile
 fi
